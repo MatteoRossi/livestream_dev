83102cb37080a74b503981b1a5189e84da44e588
Matteo Rossi <mattrossi95@gmail.com>
2024-03-26 16:09:36 -0400

    V 1.0

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, doc, onSnapshot, updateDoc, increment, collection, query, orderBy} from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  onSnapshot,
+  updateDoc,
+  increment,
+  collection,
+  query,
+  orderBy,
+} from "firebase/firestore";
 import html2canvas from "html2canvas";
 
 const firebaseConfig = {
@@ -8,7 +17,7 @@ const firebaseConfig = {
   projectId: "forum-8423f",
   storageBucket: "forum-8423f.appspot.com",
   messagingSenderId: "834054366177",
-  appId: "1:834054366177:web:45b07b4c8cc580cec968a0"
+  appId: "1:834054366177:web:45b07b4c8cc580cec968a0",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -23,71 +32,75 @@ let isLive = false;
 const qSettings = query(doc(db, "livestreams", "mll2hccPnNBua9PWcE0x"));
 
 const settings = onSnapshot(qSettings, (doc) => {
-  if(doc.data().live) {
+  if (doc.data().live) {
     isLive = true;
-    document.getElementById('video_player').innerHTML = `
+    document.getElementById("video_component").innerHTML = `
       <iframe class='iframe' src="${doc.data().stream}"
       title="YouTube video player" frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       allowfullscreen>
       </iframe>
-    `
-    document.querySelectorAll('.reactions_count').forEach(count => {
-      count.querySelector('.count').style.display = 'block';
-      count.querySelector('.placeholder').style.display = 'none';
-    })
-
-  }
-  else {
+    `;
+    document.querySelectorAll(".reactions_count").forEach((count) => {
+      count.querySelector(".count").style.display = "block";
+      count.querySelector(".placeholder").style.display = "none";
+    });
+  } else {
     isLive = false;
-    document.getElementById('video_player').innerHTML = `
+    document.getElementById("video_component").innerHTML = `
       <img class='iframe' src= "https://firebasestorage.googleapis.com/v0/b/forum-8423f.appspot.com/o/livestream%2Fposter.jpg?alt=media&token=052dca3e-e347-47bb-90a2-d26588ac1adf" />
-    `
+    `;
 
-    document.querySelectorAll('.reactions_count').forEach(count => {
-      count.querySelector('.count').style.display = 'none';
-      count.querySelector('.placeholder').style.display = 'block';
-
-    })
+    document.querySelectorAll(".reactions_count").forEach((count) => {
+      count.querySelector(".count").style.display = "none";
+      count.querySelector(".placeholder").style.display = "block";
+    });
   }
 });
 
-const reactions = onSnapshot(doc(db, "livestreams/mll2hccPnNBua9PWcE0x/reactions", "reactionsCounts"), (doc) => {
-  const data = doc.data();
+const reactions = onSnapshot(
+  doc(db, "livestreams/mll2hccPnNBua9PWcE0x/reactions", "reactionsCounts"),
+  (doc) => {
+    const data = doc.data();
 
-  Object.keys(data).forEach(key => {
-    document.querySelector(`#reactions_${key}_count`).innerHTML = `${data[key]}`;
-    if (data[key] > lastReactionsCount[key] && !userTriggeredUpdate && !isFirstLoad) {
-      createAndAnimateEmoji(key);
-      triggerContentChangedEvent(`reactions_${key}_count`);
+    Object.keys(data).forEach((key) => {
+      document.querySelector(
+        `#reactions_${key}_count`
+      ).innerHTML = `${data[key]}`;
+      if (
+        data[key] > lastReactionsCount[key] &&
+        !userTriggeredUpdate &&
+        !isFirstLoad
+      ) {
+        createAndAnimateEmoji(key);
+        triggerContentChangedEvent(`reactions_${key}_count`);
+      }
+      //also triggercontentchanged event when user triggered update
+      if (data[key] > lastReactionsCount[key] && userTriggeredUpdate) {
+        triggerContentChangedEvent(`reactions_${key}_count`);
+      }
+      lastReactionsCount[key] = data[key];
+    });
+
+    isFirstLoad = false;
+
+    // Reset the userTriggeredUpdate flag after handling the update
+    if (userTriggeredUpdate) {
+      userTriggeredUpdate = false;
     }
-    //also triggercontentchanged event when user triggered update
-    if (data[key] > lastReactionsCount[key] && userTriggeredUpdate) {
-      triggerContentChangedEvent(`reactions_${key}_count`);
-    }
-    lastReactionsCount[key] = data[key];
-    
-  });
-
-  isFirstLoad = false;
-
-  // Reset the userTriggeredUpdate flag after handling the update
-  if (userTriggeredUpdate) {
-    userTriggeredUpdate = false;
   }
-});
+);
 
 /* CONTENT HANLDER */
 
-/* VIDEO REACTION COMPONENT */
-document.getElementById('video_reaction_component').innerHTML = `
-<div id="video_player">
+/* INSERT COMPONENT */
+document.getElementById("video_component").innerHTML = `
 <img
   class="iframe"
   src="https://firebasestorage.googleapis.com/v0/b/forum-8423f.appspot.com/o/livestream%2Fposter.jpg?alt=media&token=052dca3e-e347-47bb-90a2-d26588ac1adf"
 />
-</div>
-<div id="reaction_board">
+`;
+document.getElementById("reaction_component").innerHTML = `
 <p class="reaction_text">Click to react to the speech <span style="font-weight: 900">LIVE</span>!</p>
 <div class="reactions">
   <div class="reaction" id="reactions_cheer">
@@ -118,10 +131,8 @@ document.getElementById('video_reaction_component').innerHTML = `
       <button name="love" class="reaction_button">😍</button>
   </div>
 </div>
-</div>
-`
-
-document.getElementById('bingo_component').innerHTML = `
+`;
+document.getElementById("bingo_component").innerHTML = `
 <div id="bingo_export">
 <div class="bingo-header">
 <div><span style='color:#d71920'>BUDGET</span> BINGO </div><div>
@@ -684,20 +695,19 @@ padding:0 0.3em;'>2024</span></span></div>
   <h3>You got a BINGO!</h3>
   <a id='share-sheet-button' class="share-button">Share your <b>#BudgetBingo</b> sheet on social!</a>
 </div>
-`
+`;
 
 /* EMOJI CLICK HANDLER */
 
 let lastClickTime = 0;
-const debounceInterval = 500; 
+const debounceInterval = 500;
 
-document.querySelectorAll('.reaction_button').forEach(button => {
-  button.addEventListener('click', () => {
-
+document.querySelectorAll(".reaction_button").forEach((button) => {
+  button.addEventListener("click", () => {
     const currentTime = Date.now();
 
     if (currentTime - lastClickTime < debounceInterval) {
-      console.log('Too many clicks, try again later.');
+      console.log("Too many clicks, try again later.");
       return; // Exit the function if clicked too quickly
     }
 
@@ -706,42 +716,47 @@ document.querySelectorAll('.reaction_button').forEach(button => {
     const reaction = button.name;
     userTriggeredUpdate = true; // Set the flag to indicate a user-triggered update
     createAndAnimateEmoji(reaction, true); // Now passing a flag to indicate user-triggered
-    
-    const reactionsRef = doc(db, "livestreams/mll2hccPnNBua9PWcE0x/reactions", "reactionsCounts");
+
+    const reactionsRef = doc(
+      db,
+      "livestreams/mll2hccPnNBua9PWcE0x/reactions",
+      "reactionsCounts"
+    );
     updateDoc(reactionsRef, {
-      [reaction]: increment(1)
+      [reaction]: increment(1),
     });
   });
 });
 
 function createAndAnimateEmoji(emojiType, isUserTriggered = false) {
   var emojiList = {
-    "like": '👍',
-    "love": '😍', 
-    "cheer": '🥳'
+    like: "👍",
+    love: "😍",
+    cheer: "🥳",
   };
 
-  var emoji = document.createElement('div');
+  var emoji = document.createElement("div");
   emoji.textContent = emojiList[emojiType]; // Random emoji
-  emoji.style.position = 'absolute';
-  emoji.style.bottom = '0';
+  emoji.style.position = "absolute";
+  emoji.style.bottom = "0";
   emoji.style.left = `${Math.random() * 20}%`; // Random starting position
-  emoji.style.fontSize = '24px';
-  document.getElementById('video_player').appendChild(emoji);
+  emoji.style.fontSize = "24px";
+  document.getElementById("video_component").appendChild(emoji);
 
   // customize emoji if user triggered
   if (isUserTriggered) {
     //add a "you" badge to the emoji
-    var badge = document.createElement('div');
-    badge.textContent = 'You';
-    badge.style.position = 'absolute';
-    badge.style.bottom = '-20px';
-    badge.style.left = '0px';
-    badge.style.fontSize = '12px';
-    badge.style.color = 'white';
-    badge.style.backgroundColor = 'red';
-    badge.style.padding = '2px 4px';
-    badge.style.borderRadius = '4px';
+    var badge = document.createElement("div");
+    badge.textContent = "You";
+    badge.style.position = "absolute";
+    badge.style.bottom = "-20px";
+    badge.style.left = "0px";
+    badge.style.fontSize = "12px";
+    badge.style.color = "white";
+    badge.style.backgroundColor = "red";
+    badge.style.padding = "2px 4px";
+    badge.style.borderRadius = "4px";
+    badge.style.fontFamily = "Poppins";
     emoji.appendChild(badge);
   }
 
@@ -758,22 +773,22 @@ function createAndAnimateEmoji(emojiType, isUserTriggered = false) {
     // Custom easing: faster in the first 30%, then normal speed
     var progress;
     if (normalizedTimeElapsed < 0.3) {
-        // Accelerate progress in the first 30%
-        // Adjust the multiplier to control the acceleration
-        progress = normalizedTimeElapsed * 2; // Example: make it move faster
+      // Accelerate progress in the first 30%
+      // Adjust the multiplier to control the acceleration
+      progress = normalizedTimeElapsed * 2; // Example: make it move faster
     } else {
-        // After 30%, continue at normal pace, adjusting for the initial boost
-        // The magic number 0.6 balances out the accelerated part to ensure smooth transition
-        progress = 0.6 + (normalizedTimeElapsed - 0.3);
+      // After 30%, continue at normal pace, adjusting for the initial boost
+      // The magic number 0.6 balances out the accelerated part to ensure smooth transition
+      progress = 0.6 + (normalizedTimeElapsed - 0.3);
     }
 
     if (progress < 1) {
-        emoji.style.bottom = `${progress * 100}%`; // Move emoji up
-        requestAnimationFrame(animate);
+      emoji.style.bottom = `${progress * 100}%`; // Move emoji up
+      requestAnimationFrame(animate);
     } else {
-        emoji.remove(); // Remove emoji after it reaches the top
+      emoji.remove(); // Remove emoji after it reaches the top
     }
-}
+  }
 
   requestAnimationFrame(animate);
 }
@@ -781,87 +796,85 @@ function createAndAnimateEmoji(emojiType, isUserTriggered = false) {
 /* EMOJI COUNT ANIMATION HANDLER */
 
 function triggerContentChangedEvent(buttonId) {
-  const event = new CustomEvent('contentchanged');
+  const event = new CustomEvent("contentchanged");
   document.getElementById(buttonId).dispatchEvent(event);
 }
 
 // Function to add the animation class and remove it after the animation ends
 function animateButton(button) {
-  button.classList.add('button-animate');
-  button.addEventListener('animationend', () => {
-    button.classList.remove('button-animate');
+  button.classList.add("button-animate");
+  button.addEventListener("animationend", () => {
+    button.classList.remove("button-animate");
   });
 }
-
 
 /* BINGO SHEET */
 
 const winningCombinations = [
-  ['1', '2', '3'], // First row
-  ['4', '5', '6'], // Second row
-  ['7', '8', '9'], // Third row
-  ['1', '4', '7'], // First column
-  ['2', '5', '8'], // Second column
-  ['3', '6', '9'], // Third column
-  ['1', '5', '9'], // Diagonal from top-left to bottom-right
-  ['3', '5', '7']  // Diagonal from top-right to bottom-left
+  ["1", "2", "3"], // First row
+  ["4", "5", "6"], // Second row
+  ["7", "8", "9"], // Third row
+  ["1", "4", "7"], // First column
+  ["2", "5", "8"], // Second column
+  ["3", "6", "9"], // Third column
+  ["1", "5", "9"], // Diagonal from top-left to bottom-right
+  ["3", "5", "7"], // Diagonal from top-right to bottom-left
 ];
 
-document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.bingo-section');
-  const bingoMessage = document.getElementById('bingo-message');
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".bingo-section");
+  const bingoMessage = document.getElementById("bingo-message");
 
-  sections.forEach(section => {
-    section.addEventListener('click', () => {
-      section.querySelector('.active').classList.toggle('show');
+  sections.forEach((section) => {
+    section.addEventListener("click", () => {
+      section.querySelector(".active").classList.toggle("show");
       checkForBingo();
     });
   });
 
   function checkForBingo() {
     for (let combination of winningCombinations) {
-      const hasBingo = combination.every(sectionId => {
-        return document.querySelector(`[data-section="${sectionId}"]`).querySelector('.active').classList.contains('show');
+      const hasBingo = combination.every((sectionId) => {
+        return document
+          .querySelector(`[data-section="${sectionId}"]`)
+          .querySelector(".active")
+          .classList.contains("show");
       });
 
       if (hasBingo) {
-        bingoMessage.style.display = 'flex'; // Show "BINGO!" message
+        bingoMessage.style.display = "flex"; // Show "BINGO!" message
         break; // Stop checking after finding a Bingo
-      }
-      else{
-        bingoMessage.style.display = 'none';
+      } else {
+        bingoMessage.style.display = "none";
       }
     }
   }
-
-
 });
 
-document.getElementById('share-sheet-button').addEventListener('click', () => {
-  html2canvas(document.getElementById('bingo_export')).then(function(canvas) {
+document.getElementById("share-sheet-button").addEventListener("click", () => {
+  html2canvas(document.getElementById("bingo_export")).then(function (canvas) {
     //download the image
-
-    if(navigator.canShare && (navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    )) {
-      var blob = new Blob([canvas.toDataURL()], {type: 'image/png'});
-      var file = new File([blob], 'bingo.png', {type: 'image/png'});
+    if (
+      navigator.canShare &&
+      (navigator.userAgent.match(/Android/i) ||
+        navigator.userAgent.match(/webOS/i) ||
+        navigator.userAgent.match(/iPhone/i) ||
+        navigator.userAgent.match(/iPad/i) ||
+        navigator.userAgent.match(/iPod/i) ||
+        navigator.userAgent.match(/BlackBerry/i))
+    ) {
+      var blob = new Blob([canvas.toDataURL()], { type: "image/png" });
+      var file = new File([blob], "bingo.png", { type: "image/png" });
 
       navigator.share({
         files: [file],
-        text: 'Check out my #BudgetBingo sheet!'
-      })    
-    }
-    else{
-      var link = document.createElement('a');
-      link.download = 'bingo.png';
+        text: "Check out my #BudgetBingo sheet!",
+      });
+    } else {
+      var link = document.createElement("a");
+      link.download = "bingo.png";
       link.href = canvas.toDataURL();
       link.click();
     }
-    
+  });
 });
-})
